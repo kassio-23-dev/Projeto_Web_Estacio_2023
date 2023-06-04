7fa9164255b4e59f7741f72be48edcb32a43565e
RivysonClaudio <100952904+RivysonClaudio@users.noreply.github.com>
2023-06-04 20:04:52 -0300

    SinfUp em php

---
+++ php/singup.php
@@ -0,0 +1,21 @@
+<?php
+
+if(isset($_POST['submit'])){
+
+    if ($_POST['senha'] == $_POST['senha_confirm']){
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $telefone = $_POST['telefone'];
+        $sexo = $_POST['genero'];
+        $dt_nasc = $_POST['data_nascimento'];
+        $endereco = $_POST['endereco'];
+        $senha = $_POST['senha'];
+
+        require_once '../php/conexao.php';
+
+        SING_UP($nome, $email, $endereco, $senha, $sexo, $dt_nasc, $telefone);
+        
+    }else{
+        echo '<script>alert("As senhas não conferem, tente novamente!")</script>';
+    }    
+}
