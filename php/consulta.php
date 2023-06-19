0a0cf01660497b7d69b4d5bfe8ce193d9dd01545
kassio-23-dev <132306006+kassio-23-dev@users.noreply.github.com>
2023-06-19 19:20:49 -0300

    Update consulta.php

--- php/consulta.php
+++ php/consulta.php
@@ -32,39 +32,39 @@ echo '
         <tbody>
             <tr>
                 <td class="width10percent"><label for="ID">ID</label></td>
-                <td><input class="dados-campo nao-clique" id="ID" type="text" placeholder="ID" value="'. $consulta['id_cadastro'] .'" readonly></td>
+                <td><input class="dados-campo nao-clique" id="ID" type="text" placeholder="ID" value="'. $consulta['id_cadastro'] .'" readonly required></td>
             </tr>
             <tr>
                 <td class="width10percent"><label for="nome">NOME</label></td>
-                <td><input class="dados-campo" id="nome" name="nome" type="text" placeholder="NOME" value="'. $consulta['nome_completo'] .'" readonly="true"></td>
+                <td><input class="dados-campo" id="nome" name="nome" type="text" placeholder="NOME" value="'. $consulta['nome_completo'] .'" readonly="true" required></td>
             </tr>
             <tr>
                 <td class="width10percent"><label for="senha">SENHA</label></td>
-                <td><input class="dados-campo" id="senha" name="senha" type="text" placeholder="SENHA" value="'. $consulta['senha'] .'" readonly="true"></td>
+                <td><input class="dados-campo" id="senha" name="senha" type="text" placeholder="SENHA" value="'. $consulta['senha'] .'" readonly="true" required></td>
             </tr>
             <tr>
                 <td class="width10percent"><label for="email">EMAIL</label></td>
-                <td><input class="dados-campo" id="email" name="email" type="email" placeholder="EMAIL" value="'. $consulta['email'] .'" readonly="true"></td>
+                <td><input class="dados-campo" id="email" name="email" type="email" placeholder="EMAIL" value="'. $consulta['email'] .'" readonly="true" required></td>
             </tr>
             <tr>
                 <td class="width10percent"><label for="telefone">TELEFONE</label></td>
-                <td><input class="dados-campo" id="telefone" name="telefone" type="text" placeholder="TELEFONE" value="'. $consulta['telefone'] .'" readonly="true"></td>
+                <td><input class="dados-campo" id="telefone" name="telefone" type="text" placeholder="TELEFONE" value="'. $consulta['telefone'] .'" readonly="true" required></td>
             </tr>
             <tr>
             <td class="width10percent"><label for="sexo">SEXO</label></td>
                 <td>
-                    <select id="sexo" name="sexo" class="dados-campo" disabled>
+                    <select id="sexo" name="sexo" class="dados-campo" required disabled>
                         ' . $sexo . '
                     </select>
                 </td>
             </tr>
             <tr>
                 <td class="width10percent"><label for="data-de-nascimento">DATA DE NASCIMENTO</label></td>
-                <td><input class="dados-campo" id="data-de-nascimento" name="data-de-nascimento"" type="date" placeholder="DATA DE NASCIMENTO" value="'. $consulta['data_de_nascimento'] .'" readonly="true"></td>
+                <td><input class="dados-campo" id="data-de-nascimento" name="data-de-nascimento"" type="date" placeholder="DATA DE NASCIMENTO" value="'. $consulta['data_de_nascimento'] .'" readonly="true" required></td>
             </tr>
             <tr>
                 <td class="width10percent"><label for="ENDERECO">ENDEREÇO</label></td>
-                <td><input class="dados-campo" id="endereco" name="endereco" type="text" placeholder="ENDEREÇO" value="'. $consulta['endereco'] .'" readonly="true"></td>
+                <td><input class="dados-campo" id="endereco" name="endereco" type="text" placeholder="ENDEREÇO" value="'. $consulta['endereco'] .'" readonly="true" required></td>
             </tr>
 
             <tr class="botao-mobile">
